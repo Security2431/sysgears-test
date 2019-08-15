de7003931b8daa2fba846986f67e4af8fd39870a
Artem Shcherbakov <Artem2431@gmail.com>
2019-08-15 22:15:36 +0200

    feat: configs react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
-function App() {
-  return (
-    <>
-      <div className="App">
-        <header className="App-header" />
-      </div>
-    </>
-  )
-}
+import {
+  Router, Route, Switch, Redirect,
+} from 'react-router-dom'
+
+import Task1 from './Task1'
+import Task2 from './Task2'
+import Task3 from './Task3'
+import Task4 from './Task4'
+import history from '../utils/history'
+
+const App = () => (
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" component={Task1} />
+      <Route path="/task2" component={Task2} />
+      <Route path="/task3" component={Task3} />
+      <Route path="/task4" component={Task4} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+)
 
 export default App
